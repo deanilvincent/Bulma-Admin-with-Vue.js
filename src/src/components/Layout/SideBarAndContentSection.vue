0744f69b91cf7971f51f3485d475bd29336071e3
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-10-29 20:59:54 +0800

    update overall layout

--- src/src/components/Layout/SideBarAndContentSection.vue
+++ src/src/components/Layout/SideBarAndContentSection.vue
@@ -1,119 +1,60 @@
 <template>
-    <!-- <div>
-            <div class="columns">
-                <aside class="column is-2 is-fullheight is-narrow-mobile is-hidden-mobile" id="sidebarStyle">
-                <br />
-                <nav class="menu">
-                    <ul class="menu-list">
-                    <li><a class="is-active">Dashboard</a></li>
-                    <li><a>Customers</a></li>
-                    </ul>
-                    <p class="menu-label">
-                    Administration
-                    </p>
-                    <ul class="menu-list">
-                    <li><a>Team Settings</a></li>
-                    <li>
-                        <a class="">Manage Your Team</a>
-                        <ul>
-                        <li><a>Members</a></li>
-                        <li><a>Plugins</a></li>
-                        <li><a>Add a member</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Invitations</a></li>
-                    <li><a>Cloud Storage Environment Settings</a></li>
-                    <li><a>Authentication</a></li>
-                    </ul>
-                    <p class="menu-label">
-                    Transactions
-                    </p>
-                    <ul class="menu-list">
-                    <li><a>Payments</a></li>
-                    <li><a>Transfers</a></li>
-                    <li><a>Balance</a></li>
-                    </ul>
-                </nav>
-                </aside>
-                
-                <main class="column">
-                    <router-view></router-view>
-                </main>
-            </div>
-
-    </div> -->
-    
-    <section class="main-content columns is-fullheight">
-    <aside class="column is-2 aside hero is-fullheight is-narrow-mobile section is-hidden-mobile" id="sidebarStyle">
-        <ul class="menu-list">
-        <li>
-            <a @click="navHome(activeLink = 'linkHome')" :class="{'is-active': activeLink === 'linkHome'}" id="linkStyle">
-            <span class="icon"><i class="fa fa-dashboard"></i></span> Home
-            </a>
-        </li>
-        <li>
-            <a href="#" @click="navAbout(activeLink = 'linkAbout')" :class="{'is-active': activeLink === 'linkAbout'}" id="linkStyle">
-            <span class="icon"><i class="fa fa-table"></i></span> Links
-            </a>
-
-            <ul>
-            <li>
-                <a href="#" id="linkStyle">
-                <span class="icon is-small"><i class="fa fa-link"></i></span> Link1
-                </a>
-            </li>
-            <li>
-                <a href="#" id="linkStyle">
-                <span class="icon is-small"><i class="fa fa-link"></i></span> Link2
-                </a>
-            </li>
-            </ul>
-        </li>
-        <li>
-            <a href="#" id="linkStyle">
-            <span class="icon"><i class="fa fa-info"></i></span> About
-            </a>
-        </li>
+<div>
+    <div :class="sideBarClass">
+        <ul>
+            <li><a href="#"><i class="fa fa-dashboard"></i><span>Dashboard</span></a></li>
+            <li><a href="#"><i class="fa fa-server"></i><span>Server</span></a></li>
+            <li><a href="#"><i class="fa fa-calendar"></i><span>Calendar</span></a></li>
+            <li><a href="#"><i class="fa fa-envelope-o"></i><span>Messages</span></a></li>
+            <li><a href="#"><i class="fa fa-table"></i><span>Data Table</span></a></li>
+            <li><a href="#" @click="openAndCloseSideBar"><i class="fa fa-arrows-h"></i><span>Close Sidebar</span></a></li>
         </ul>
-    </aside>
-        <div id="sectionStyle">
-            <br />
-            <router-view></router-view>
-        </div>
-    </section>
+    </div>
+    <!-- Content -->
+    <div :class="mainClass">
+        <router-view></router-view>
+    </div>
+</div>
 </template>
 <script>
 export default {
     data(){
         return{
-            activeLink: ""
+            sideBarClass: "sidebar active",
+            mainClass: "main active"
         }
     },
     methods:{
-        navHome(args){
-            this.$router.push({name: 'home'});
-            return args;
+        openAndCloseSideBar(){
+            if(this.sideBarClass === "sidebar active" && this.mainClass === "main active"){
+                this.sideBarClass = "sidebar";
+                this.mainClass = "main";
+            }else{
+                this.sideBarClass = "sidebar active";
+                this.mainClass = "main active";
+            }
         },
-        navAbout(args){
-            this.$router.push({name: 'about'});
-            return args;
+        windowSizeTrigger(){
+            if(window.innerWidth <= 1087){
+                this.sideBarClass = "sidebar";
+                this.mainClass = "main";
+            }else{
+                this.sideBarClass = "sidebar active";
+                this.mainClass = "main active";
+            }
         }
+    },
+    created(){
+        if(/Android|webOS|iPhone|iPad|iPod|pocket|psp|kindle|avantgo|blazer|midori|Tablet|Palm|maemo|plucker|phone|BlackBerry|symbian|IEMobile|mobile|ZuneWP7|Windows Phone|Opera Mini/i.test(navigator.userAgent)) {
+            this.sideBarClass = "sidebar";
+            this.mainClass = "main";
+        }
+        window.addEventListener('resize', this.windowSizeTrigger);
+        this.windowSizeTrigger();
     }
 };
 </script>
 
-<style>
-#sidebarStyle{
-    box-shadow: 0px 3px 5px #ccc;
-    position: fixed;
-    float: left;
-    width: 15%;
-}
-#sectionStyle{
-    float:left;
-    width: 85%;
-}
-#linkStyle{
-    font-size: 14px;
-}
+<style lang="less">
+@import '../../custom-css/less/admin.less';
 </style>
